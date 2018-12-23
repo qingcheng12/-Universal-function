591aa296e3c50e292cb68c5bfd36a515285312b0
qingcheng12 <32449926+qingcheng12@users.noreply.github.com>
2018-12-23 11:56:22 +0800

    Create get_time_func.c
    获取当前时间：用微秒和秒表示
    Get the current time: expressed in microseconds and seconds

---
+++ get_time_func.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <inttypes.h>
+
+// get microseconds of current time
+unsigned long current_time(void){
+	unsigned long   int         us; // Microseconds
+  time_t          s;  // Seconds
+	struct timespec spec;
+	clock_gettime(CLOCK_REALTIME, &spec);
+	s  = spec.tv_sec;
+  
+  // Convert nanoseconds to microseconds
+  us = /*round*/(spec.tv_nsec / 1.0e3); 
+  
+  return us;
+}
+
+
+// get seconds of current time
+unsigned long getsecs(void){
+
+	unsigned long            us; // Microseconds
+  time_t          s;  // Seconds
+	struct timespec spec;
+	clock_gettime(CLOCK_REALTIME, &spec);
+	s  = spec.tv_sec;
+	return s;
+
+}
+
+
+int main(int argc, char** argv)
+{
+   current_time();
+   getsecs();
+
+  return 0;
+}
+
+
+
+
