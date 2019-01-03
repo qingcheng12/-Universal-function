82d292fea30d8b4caead1043d03b114fe24c793a
qingcheng12 <32449926+qingcheng12@users.noreply.github.com>
2019-01-03 17:12:49 +0800

    Update pthread_bind_cpu.c

--- pthread_bind_cpu.c
+++ pthread_bind_cpu.c
@@ -1,5 +1,16 @@
 /*  Compile : gcc pthread_bind_cpu.c -lpthread */
 
+/*
+//初始化，设为空
+void CPU_ZERO (cpu_set_t *set); 
+//将某个cpu加入cpu集中 
+void CPU_SET (int cpu, cpu_set_t *set); 
+//将某个cpu从cpu集中移出 
+void CPU_CLR (int cpu, cpu_set_t *set); 
+//判断某个cpu是否已在cpu集中设置了 
+int CPU_ISSET (int cpu, const cpu_set_t *set); 
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
